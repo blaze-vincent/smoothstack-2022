82c6d762dfe614c82b88ee7521046bdd5f6c4eb8
blaze@drakehs <blaze.vincent@drakehs.com>
2022-11-16 11:50:31 -0600

    Match reference website font

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,7 @@ class MyDocument extends Document {
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-            rel="stylesheet"
-          />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" /> 
         </Head>
         <body>
           <Main />
