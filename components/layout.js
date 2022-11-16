02d3d8220ffa6041dd784e14c909055935094a41
blaze@drakehs <blaze.vincent@drakehs.com>
2022-11-16 10:40:27 -0600

    Add favicons; Change layout style

--- components/layout.js
+++ components/layout.js
@@ -3,17 +3,20 @@ import Header from './header'
 import Footer from './footer'
 
 export default function Layout({children}){
-  return (<>
+  return (<div className="bg-neutral-900 relative">
     <Head>
-      <title>nextjs w/ tailwindcss</title>
+      <title>Smoothstack</title>
       <meta name="description" content="bv nextjs template with tailwindcss" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     </Head>
 
     <Header />
 
-    <main>{children}</main>
+    <main className="h-screen pt-40">{children}</main>
 
     <Footer />
-    </>)
+  </div>)
 }
