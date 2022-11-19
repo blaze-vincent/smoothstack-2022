31db81fb5556087e078e825fb6373e7d06d66d39
bv laptop <blaze.vincent@hotmail.com>
2022-11-19 17:02:41 -0600

    Clarify measurement unit

--- components/header/logo.js
+++ components/header/logo.js
@@ -6,7 +6,7 @@ export default function Logo({widthOverride}){
   return <div 
     className={`w-[25rem] max-w-full`}
     style={ widthOverride ? {
-      width: widthOverride * 16
+      width: widthOverride * 16 //px
     } : null}
   >
     <Link href='/'>
