12b28d12803af8c004fa43056f0fec3789029cd8
bv laptop <blaze.vincent@hotmail.com>
2022-11-19 17:06:42 -0600

    Add internal page for CRUD on job listings

---
+++ pages/internal.js
@@ -0,0 +1,52 @@
+import SignInForm from "../components/form/signInForm";
+import CreateJobListingForm from "../components/form/createJobListingForm";
+import Hero from "../components/hero/hero";
+import { useEffect, useState } from "react";
+import JobListings from "../components/jobListings/jobListings";
+
+export default function Internal(){
+  const [signedIn, setSignedIn] = useState(false)
+  const [jobs, setJobs] = useState([])
+  const [jobsNeedUpdated, setJobsNeedUpdated] = useState(true)
+
+  useEffect(() => {
+    fetch('/api/auth', {
+      method: 'POST',
+      body: new FormData()
+    }).then(res => {
+      setSignedIn(res.ok)
+    })
+  }, [])
+
+  useEffect(() => {
+    if(jobsNeedUpdated){
+      fetch('/api/joblistings').then(res => {
+        return res.json()
+      }).then(res => {
+        setJobs(res.jobListings)
+      })
+    }
+    setJobsNeedUpdated(false)
+  }, [jobsNeedUpdated])
+
+  return <div>
+    <Hero 
+      backgroundMedia={{
+        type: 'image',
+        src: '/ideasman.jpg'
+      }}
+      h1Content='SMOOTHSTACK INTRANET'
+      h2Content='Create, update, and delete job listings'
+    />
+    {signedIn ? <div>
+      <CreateJobListingForm updateFlag={setJobsNeedUpdated} /> 
+      {
+        jobs ? <JobListings 
+          jobs={jobs} 
+          updateFlag={setJobsNeedUpdated}
+          admin={true}
+        /> : null
+      }  
+    </div>: <SignInForm onSignIn={setSignedIn} />}
+  </div>
+}
